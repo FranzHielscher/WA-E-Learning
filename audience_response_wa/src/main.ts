817901f4c6cf0b0e59ba43d556c582779b53bc04
Thomas Mugler <thomas.mugler@stud.htwk-leipzig.de>
2024-06-10 10:13:43 +0200

    feat: conference.tmj entries/exits/starts

--- audience_response_wa/src/main.ts
+++ audience_response_wa/src/main.ts
@@ -12,18 +12,20 @@ WA.onInit()
     console.log("Scripting API ready");
     console.log("Player tags: ", WA.player.tags);
 
-    WA.room.area.onEnter("clock").subscribe(() => {
-      const today = new Date();
-      const time = today.getHours() + ":" + today.getMinutes();
-      currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    WA.room.area.onEnter("JitsiMeeting1").subscribe(() => {
+      currentPopup = WA.ui.openPopup("JitsiMeetingPopup", "Welcome to Jitsi!", []);
     });
+    WA.room.area.onLeave("JitsiMeeting1").subscribe(closePopup);
 
-    WA.room.area.onLeave("clock").subscribe(closePopup);
-
-    WA.room.area.onEnter("jitsiChillzone").subscribe(() => {
-      currentPopup = WA.ui.openPopup("jitsiPopup", "Hey it works", []);
+    WA.room.area.onEnter("JitsiMeeting2").subscribe(() => {
+      currentPopup = WA.ui.openPopup("JitsiMeetingPopup", "Welcome to Jitsi!", []);
     });
-    WA.room.area.onLeave("jitsiChillzone").subscribe(closePopup);
+    WA.room.area.onLeave("JitsiMeeting2").subscribe(closePopup);
+
+    WA.room.area.onEnter("JitsiMeeting3").subscribe(() => {
+      currentPopup = WA.ui.openPopup("JitsiMeetingPopup", "Welcome to Jitsi!", []);
+    });
+    WA.room.area.onLeave("JitsiMeeting3").subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra()
